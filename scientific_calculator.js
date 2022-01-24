bf959570cc895339dd9ca939df2deba2dd5c7541
hardik <hardik@gmail.com>
2022-01-24 20:21:37 +0530

    JS Practical completed

--- scientific_calculator.js
+++ scientific_calculator.js
@@ -1,175 +1,340 @@
 let display = document.getElementById("display");
-// display.innerText = "0";
+let s_display = document.getElementById("sub_display");
 let buttons = Array.from(document.getElementsByClassName("btn"));
 let btn_sci = Array.from(document.getElementsByClassName("trigino_&_Fun"));
-const facto = (num) => {
+let memory_btn = Array.from(document.getElementsByClassName("memory"));
+
+
+var arr_list = []
+function memory(m_fun, data) {
+  if (m_fun == "M+") {
+    arr_list.push(data);
+    display.value = data;
+    s_display.value = `M+(${display.value})`;
+  } else if (m_fun == "M-") {
+    arr_list.push(eval('-' + data));
+    display.value = data;
+    s_display.value = `M-(${display.value})`;
+  } else if (m_fun == "MS") {
+    arr_list.push(data);
+    display.value = arr_list;
+    s_display.value = `MS(${display.value})`;
+  } else if (m_fun == "MC") {
+    arr_list = [];
+    display.value = arr_list;
+    s_display.value = `MC`;
+  } else {
+    let add_data = 0;
+    arr_list.forEach((data) => {
+      add_data += data
+    });
+    display.value = add_data;
+    s_display.value = `Mr(${display.value})`;
+  }
+}
+
+const deg_rad = () => {
+  let val = document.getElementById("deg");
+  let convert = parseInt(display.value);
+  if (display.value !== "0" && display.value !== "") {
+    if (val.innerText == "DEG") {
+      s_display.value = `Deg (${display.value})`;
+      val.innerText = "RAD";
+      display.value = convert * (180 / Math.PI);
+    } else {
+      val.innerText = "DEG";
+      s_display.value = `Rad (${display.value})`;
+      display.value = convert * (Math.PI / 180);
+    }
+  }
+}
+
+const Review = () => {
+  display.value = s_display.value;
+}
+
+const factorial = (num) => {
   fact = 1;
   for (let i = 1; i <= num; i++) {
     fact *= i;
   }
   return fact;
 };
+
 const del = () => {
-  let dele = display.innerText;
-  display.innerText = dele.substring(0, dele.length - 1);
+  s_display.value = "";
+  let dele = display.value;
+  display.value = dele.substring(0, dele.length - 1);
 };
 
+const check = (val) => {
+  let isvalid;
+  let char_list = ["+", "-", "/", "*", "%", "!", "^", "π", "e", "|x|"];
+  let last_char = val.charAt(val.length - 1);
+  if (display.value !== "0" && display.value !== "") {
+    if (char_list.includes(last_char)) {
+      isvalid = false;
+    } else {
+      isvalid = true;
+    }
+    return isvalid;
+  }
+};
+
+
+
+memory_btn.map((value) => {
+  value.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "MC":
+        memory("MC", eval(display.value));
+        break;
+      case "Mr":
+        memory("Mr", eval(display.value));
+        break;
+      case "M+":
+        memory("M+", eval(display.value));
+        break;
+      case "M-":
+        memory("M-", eval(display.value));
+        break;
+      case "MS":
+        memory("MS", eval(display.value));
+        break;
+    }
+  });
+});
+
+
 btn_sci.map((value) => {
-    value.addEventListener("click", (a) => {
-      console.log(a.target.innerText);
+  value.addEventListener("click", (a) => {
+    if (display.value !== "0" && display.value !== "") {
       switch (a.target.innerText) {
         case "sin":
-          display.innerText = Math.sin(display.innerText);
+          s_display.value = `sin(${display.value})`;
+          display.value = Math.sin(display.value);
           break;
         case "cos":
-          display.innerText = Math.cos(display.innerText);
+          s_display.value = `cos(${display.value})`;
+          display.value = Math.cos(display.value);
           break;
         case "tan":
-          display.innerText = Math.tan(display.innerText);
+          s_display.value = `tan(${display.value})`;
+          display.value = Math.tan(display.value);
+          break;
+        case "asin":
+          s_display.value = `asin(${display.value})`;
+          display.value = Math.asin(display.value);
+          break;
+        case "acosh":
+          s_display.value = `acosh(${display.value})`;
+          display.value = Math.acosh(display.value);
+          break;
+        case "atan":
+          s_display.value = `atan(${display.value})`;
+          display.value = Math.atan(display.value);
+          break;
+        case "ceil":
+          s_display.value = `ceil(${display.value})`;
+          display.value = Math.ceil(display.value);
+          break;
+        case "floor":
+          s_display.value = `floor(${display.value})`;
+          display.value = Math.floor(display.value);
+          break;
+        case "random":
+          s_display.value = `ramdom(${display.value})`;
+          display.value = Math.random(display.value);
           break;
       }
-    });
+    }
   });
+});
 
-buttons.map((button) => {
-  button.addEventListener("click", (e) => {
+
+
+buttons.map((value) => {
+  value.addEventListener("click", (e) => {
     switch (e.target.innerText) {
       case "=":
-        if (display.innerText.charAt(display.innerText.length - 1) == "!") {
-          display.innerText = display.innerText.substring(
+        s_display.value = `${display.value}=`;
+        if (display.value.charAt(display.value.length - 1) == "!") {
+          display.value = display.value.substring(
             0,
-            display.innerText.length - 1
+            display.value.length - 1
           );
-          display.innerText = facto(display.innerText);
+          display.value = factorial(display.value);
         }
-        if (display.innerText.includes("^")) {
-          let sign = display.innerText.indexOf("^");
-          let a = display.innerText.substring(0, sign);
-          let b = display.innerText.substring(
+        if (display.value.includes("^")) {
+          let sign = display.value.indexOf("^");
+          let x = display.value.substring(0, sign);
+          let y = display.value.substring(
             sign + 1,
-            display.innerText.length
+            display.value.length
           );
-          display.innerText = Math.pow(a, b);
+          display.value = Math.pow(x, y);
         }
-        display.innerText = eval(display.innerText);
+        display.value = eval(display.value);
+        break;
+
+      case "F-E":
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `F-E (${display.value})`;
+          num = parseFloat(display.value);
+          display.value = num.toExponential();
+        }
         break;
 
       case "C":
-        display.innerText = "";
+        s_display.value = "";
+        display.value = "";
         break;
 
       case "n!":
         if (check(display.innerHTML)) {
-          display.innerText += "!";
+          display.value += "!";
+        }
+        break;
+
+      case "xy":
+        if (check(display.value)) {
+          display.value += "^";
+        }
+        break;
+
+      case "^":
+        if (check(display.value)) {
+          display.value = display.value.slice(0, -1);
+        }
+        break;
+
+      case "+":
+        if (check(display.value)) {
+          display.value = display.value + "+";
+        }
+        break;
+
+      case "-":
+        if (check(display.value)) {
+          display.value = display.value + "-";
+        }
+        break;
+
+      case "×":
+        if (check(display.value)) {
+          display.value = display.value + "*";
+        }
+        break;
+
+      case "÷":
+        if (check(display.value)) {
+          display.value = display.value + "/";
+        }
+        break;
+
+      case "+/-":
+        if (display.value > 0) {
+          display.value = `-${display.value}`;
+        } else {
+          display.value = Math.abs(display.value);
         }
         break;
 
       case "π":
         if (check(display.innerHTML)) {
-          display.innerText = display.innerHTML + 3.14159;
+          s_display.value = `${display.value}×π`;
+          display.value = display.value * Math.PI;
         }
         break;
 
       case "e":
         if (check(display.innerHTML)) {
-          display.innerText = display.innerText + 2.718281828459045;
+          s_display.value = `${display.value}×e`;
+          display.value = display.value * Math.E;
         }
         break;
 
       case "|x|":
-          display.innerText = Math.abs(display.innerText);
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `| ${display.value} | `;
+          display.value = Math.abs(display.value); ''
+        }
         break;
 
       case "√":
-          tmp = display.innerText;
-          display.innerText = Math.sqrt(display.innerText);
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `√${display.value}`;
+          display.value = Math.sqrt(display.value);
+        }
         break;
 
       case "x2":
-          display.innerText = display.innerText ** 2;
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `${display.value}^2`;
+          display.value = display.value ** 2;
+        }
         break;
 
       case "10x":
-          display.innerText = 10 ** display.innerText;
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `10^${display.value}`;
+          display.value = 10 ** display.value;
+        }
         break;
 
       case "1/x":
-          display.innerText = 1 / display.innerText;   
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `1/${display.value}`;
+          display.value = 1 / display.value;
+        }
         break;
-    case "xy":
-            display.innerText += "^";
-            break;
 
-      case "2nd":
-          display.innerText = 2 ** display.innerText; 
+      case "2n":
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `2^${display.value}`;
+          display.value = 2 ** display.value;
+        }
         break;
 
       case "log":
-          display.innerText = Math.log10(display.innerText);
-          break;
-    case "^":
-        if (display.innerText) {
-          display.innerText = display.innerText.slice(0, -1);
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `log(${display.value})`;
+          display.value = Math.log10(display.value);
         }
         break;
 
-    case "×":
-        if (check(display.innerText)) {
-          display.innerText = display.innerText + "*";
-        }
-        break;
-
-      case "÷":
-        if (check(display.innerText)) {
-          display.innerText = display.innerText + "/";
-        }
-        break;
-
-    
       case "ln":
-        display.innerText = Math.log(display.innerText);
-        break;
-    case "exp":
-        display.innerText = Math.exp(display.innerText);
-        break;
-    case "mod":
-        if (check(display.innerText)) {
-          display.innerText += "%";
+        if (display.value !== "0" && display.value !== "") {
+          s_display.value = `ln(${display.value})`;
+          display.value = Math.log(display.value);
         }
         break;
 
+      case "exp":
+        if (display.value !== "0" && display.value !== "") {
+          display.value = Math.exp(display.value);
+        }
+        break;
 
-    case ".":
-        if (display.innerText == null || display.innerText == "0") {
-          display.innerText = ".";
+      case "mod":
+        if (display.value !== "0" && display.value !== "") {
+          if (check(display.value)) {
+            display.value += "%";
+          }
+        }
+        break;
+
+      case ".":
+        if (display.value == null || display.value == "0") {
+          display.value = ".";
         } else {
-          display.innerText += ".";
-        }
-        break;
-
-    case "+/-":
-        if(display.innerText > 0){
-            display.innerText = `-${display.innerText}`;
-        }else{
-            display.innerText = Math.abs(display.innerText);
+          display.value += ".";
         }
         break;
 
       default:
-        display.innerText += e.target.innerText;
+        display.value += e.target.innerText;
     }
 
   });
 });
-
-const check = (val) => {
-  let isvalid;
-  let char_list = ["+", "-", "/", "*", "%", "!", "π", "e"];
-  let last_char = val.charAt(val.length - 1);
-  if (char_list.includes(last_char)) {
-    isvalid = false;
-  } else {
-    isvalid = true;
-  }
-  return isvalid;
-};
